d09385de84a609f8b5abebd868bd8bd8842e7262
Ethem Mehmet MAVİŞ <1030521076@erciyes.edu.tr>
2023-10-18 16:00:18 +0300

    Bouncing Ball
    "It's a simple level bouncing ball game made with C# and Windows Forms. The racket is moved using the Right and Left arrow keys. The difficulty increases with each ball you catch.

---
+++ Top Sektirme 2/Form1.Designer.cs
@@ -0,0 +1,153 @@
+﻿namespace Top_Sektirme_2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.top = new System.Windows.Forms.PictureBox();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.top)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // top
+            // 
+            this.top.Image = global::Top_Sektirme_2.Properties.Resources.basketbol_topu_png;
+            this.top.Location = new System.Drawing.Point(35, 199);
+            this.top.Name = "top";
+            this.top.Size = new System.Drawing.Size(50, 50);
+            this.top.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.top.TabIndex = 0;
+            this.top.TabStop = false;
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 50;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label1
+            // 
+            this.label1.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label1.Location = new System.Drawing.Point(329, 508);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(250, 16);
+            this.label1.TabIndex = 1;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(0, 542);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(869, 35);
+            this.panel1.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(85, 14);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(49, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Zorluk: 1";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 14);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Puan: 0";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(353, 211);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(198, 75);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "label4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Visible = false;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(415, 289);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "Yeni Oyun";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(866, 578);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.top);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.KeyPreview = true;
+            this.Name = "Form1";
+            this.Text = "Top Sektirme";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.top)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox top;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ Top Sektirme 2/Form1.cs
@@ -0,0 +1,137 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Top_Sektirme_2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int artisX = 10, artisY = 10, raket = 20, puanSayaci = 0, zorluk = 1;
+        bool bitir = false;
+        Random rnd = new Random();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            top.Location = new Point(rnd.Next(10, this.Width - 10), rnd.Next(10, panel1.Location.Y - this.Height / 2));
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            bitir = false;
+            timer1.Enabled = true;
+            top.Visible = true;
+            label1.Visible = true;
+            panel1.Visible = true;
+            label4.Visible = false;
+            button1.Visible = false;
+            label1.Width = 250;
+            top.Location = new Point(rnd.Next(10, this.Width - 10), rnd.Next(10, panel1.Location.Y - this.Height / 2));
+            timer1.Interval = 50;
+            puanSayaci = 0;
+            zorluk = 1;
+            label2.Text = "Puan: 0";
+            label3.Text = "Zorluk: 1";
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (label1.Location.X > 0 && label1.Location.X < (this.Width - label1.Width))
+            {
+                if (e.KeyCode == Keys.Right)
+                {
+                    raket = 20;
+                    label1.Location = new Point(label1.Location.X + raket, label1.Location.Y);
+                }
+                if (e.KeyCode == Keys.Left)
+                {
+                    raket = -20;
+                    label1.Location = new Point(label1.Location.X + raket, label1.Location.Y);
+                }
+            }
+            else if (label1.Location.X <= 0 && e.KeyCode == Keys.Right)
+            {
+                label1.Location = new Point(label1.Location.X + 20, label1.Location.Y);
+            }
+            else if (label1.Location.X >= this.Width - label1.Width && e.KeyCode == Keys.Left)
+            {
+                label1.Location = new Point(label1.Location.X - 20, label1.Location.Y);
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (top.Bottom >= label1.Top && top.Bottom < label1.Bottom)
+            {
+                if (top.Right >= label1.Left && top.Left <= label1.Right)
+                {
+                    artisY = -10;
+                    label1.Width -= 10;
+                    puanSayaci += 5;
+                    zorluk += 1;
+                    label2.Text = "Puan: " + puanSayaci;
+                    label3.Text = "Zorluk:" + zorluk;
+                    if (timer1.Interval > 50)
+                    {
+                        timer1.Interval -= 50;
+                    }
+                    else if (timer1.Interval > 10 && timer1.Interval <= 50)
+                    {
+                        timer1.Interval -= 10;
+                    }
+                }
+                else
+                {
+                    artisY = 10;
+                }
+            }
+            else if (top.Bottom > label1.Bottom + 8)
+            {
+                bitir = true;
+            }
+            else
+            {
+                if (top.Location.Y > panel1.Location.Y - top.Height)
+                {
+                    artisY = -10;
+                }
+                else if (top.Location.Y < 20)
+                {
+                    artisY = 10;
+                }
+                if (top.Location.X > this.Width - 100)
+                {
+                    artisX = -10;
+                }
+                else if (top.Location.X < 20)
+                {
+                    artisX = 10;
+                }
+            }
+
+            if (bitir == true)
+            {
+                timer1.Enabled = false;
+                top.Visible = false;
+                label1.Visible = false;
+                panel1.Visible = false;
+                label4.Visible = true;
+                button1.Visible = true;
+                label4.Text = "Oyun Bitti!\nPuanınız: " + puanSayaci;
+            }
+            else
+            {
+                top.Location = new Point(top.Location.X + artisX, top.Location.Y + artisY);
+            }
+        }
+    }
+}
+
